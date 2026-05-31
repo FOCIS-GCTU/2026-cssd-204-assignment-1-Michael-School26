f34fe74f1fd8cba35921db24995b8371ecf98526
Michael-School26 <2425403091@live.gctu.edu.gh>
2026-05-31 23:17:06 +0000

    completed

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Michael Kwame Blessing Annan
+# STUDENT ID: 2425403091
+# Email: 2425403091@live.gctu.edu.gh
+# Grader:Augustus Buckman
+#
+# On my honor,Michael Kwame Blessing Annan, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
   pass #code goes here
 
+
+    # Print initials MKBA in small form, then as large block letters
+   
+
+    print()
+    print("...MKBA")
+    print()
+    print("...M..........M........kK........Kk........BBBBBBBBB.............AAAAAAAA.......")
+    print("...MM........MM........kK.......Kkk........B.........B..........AA......AA......")
+    print("...M.M......M.M........kK......Kkk.........B..........B........AAA......AAA.....")
+    print("...M..M....M..M........kK.....Kkk..........B..........B........AAA......AAA.....")
+    print("...M...MMMM...M........KkkkkK..............BBBBBBBBBB..........AAAAAAAAAAAA.....")
+    print("...M..........M........kK....Kkk...........B..........B........AAA......AAA.....")
+    print("...M..........M........kK.....Kkk..........B..........B........AAA......AAA.....")
+    print("...M..........M........kK......Kkk.........B..........B........AAA......AAA.....")
+    print("...M..........M...**...kK.......Kkk...**...B.........B....**...AAA......AAA...**")
+    print("...M..........M...**...kK........kk...**...BBBBBBBBB......**...AAA......AAA...**")
+    print()
 
 
 main()
